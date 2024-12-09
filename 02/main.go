5b5c286344b145bfbb85e1e5d74dd5552c7cde7f
Martin Käser <kaeser@puzzle.ch>
2024-12-09 08:48:13 +0100

    02_01: WIP

--- 02/main.go
+++ 02/main.go
@@ -27,9 +27,60 @@ func runMain() error {
 		return err
 	}
 
-	fmt.Println("lists:")
-	fmt.Println(lists)
+	countSave := countSaveLists(lists)
+
+	fmt.Println("saveCount:")
+	fmt.Println(countSave)
 	return nil
+}
+
+func countSaveLists(lists [][]int) int {
+	saveCount := 0
+	for _, list := range lists {
+		isSave := isListSave(list)
+		if isSave {
+			saveCount++
+		}
+	}
+	return saveCount
+}
+
+const ASC = 1
+const DESC = 2
+const UNKNOWN = 0
+
+func isListSave(list []int) bool {
+	var previousValuePtr *int
+	previousOrder := UNKNOWN
+
+	for _, currentValue := range list {
+		if previousValuePtr == nil {
+			previousValuePtr = &currentValue
+			continue
+		}
+
+		currentOrder := UNKNOWN
+		if *previousValuePtr < currentValue {
+			currentOrder = ASC
+		} else if *previousValuePtr > currentValue {
+			currentOrder = DESC
+		}
+
+		// UNSAFE: previousValue == currentValue
+		if currentOrder == UNKNOWN {
+			return false
+		}
+
+		// UNSAFE: Ordering mismatch
+		if previousOrder != currentOrder {
+			return false
+		}
+
+		previousOrder = currentOrder
+		previousValuePtr = &currentValue
+	}
+
+	return false
 }
 
 func getDiff(i1 int, i2 int) int {
